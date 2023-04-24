bfd8484bc976839bd077a137d5fe17eb4f6d07c9
nasca55 <nasca55@naver.com>
2023-04-24 23:06:51 +0900

    2023-04-24 study reactive layout

--- study_js/js/study_js.js
+++ study_js/js/study_js.js
@@ -46,3 +46,43 @@ $('form').on('submit', function(e) {
     alert('passwd should more than 6 letters.');
   }
 });
+
+// let age = 26;
+// const born = 'namyangju';
+
+// var name = 'kang';
+// var id = 0;
+
+// function showName() {
+//   var name = 'kim';
+//   var id = 1;
+//   console.log(name);
+// }
+
+// showName();
+// console.log(id);
+
+// let deposit = prompt('예금하실 금액을 적어주세요');
+// let depositFuture = deposit;
+
+// if (deposit >= 50000) {
+//   depositFuture *= 1.4;
+// }
+// else {
+//   depositFuture *= 1.3;
+// }
+
+// console.log(depositFuture);
+
+// let first = parseInt(prompt('처음에 얼마나 마실겨?'));
+
+// console.log(`당신이 총 마실 수 있는 ml는 ${first + first * (2/3) + (first * (2/3)) * (2/3)}ml입니다.`);
+
+const input = $('.quiz-input');
+const button = $('.quiz-button');
+
+button.on('click', () => {
+  if(input.val() == '1335') {
+    alert('축하합니다.');
+  }
+})
