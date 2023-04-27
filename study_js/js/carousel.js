847c66bce4949814191bf7153d407acf54a59d9f
nasca55 <nasca55@naver.com>
2023-04-27 19:17:34 +0900

    carousel slide no limit for slide

--- study_js/js/carousel.js
+++ study_js/js/carousel.js
@@ -22,32 +22,41 @@ btn3.addEventListener('click', () => {
   absPosX = 3;
 });
 
+let max = 3;
+
+// prev.addEventListener('click', () => {
+//   if(absPosX == 1) {
+//     box.style.transform = 'translateX(-200vw)';
+//     absPosX = 3;
+//   }
+//   else if(absPosX == 2) {
+//     box.style.transform = 'translateX(-0vw)';
+//     absPosX = 1; 
+//   }
+//   else if(absPosX == 3) {
+//     box.style.transform = 'translateX(-100vw)';
+//     absPosX = 2;
+//   }
+// });
+
 prev.addEventListener('click', () => {
-  if(absPosX == 1) {
-    box.style.transform = 'translateX(-200vw)';
-    absPosX = 3;
+  if(absPosX != 1) {
+    box.style.transform = 'translateX(-' + (absPosX-2)*100 + 'vw)';
+    absPosX -= 1;
   }
-  else if(absPosX == 2) {
-    box.style.transform = 'translateX(-0vw)';
-    absPosX = 1; 
-  }
-  else if(absPosX == 3) {
-    box.style.transform = 'translateX(-100vw)';
-    absPosX = 2;
+  else {
+    absPosX = max;
+    box.style.transform = 'translateX(-' + (absPosX-1)*100 + 'vw)';
   }
 });
 
 next.addEventListener('click', () => {
-  if(absPosX == 1) {
-    box.style.transform = 'translateX(-100vw)';
-    absPosX = 2;
+  if(absPosX != max) {
+    box.style.transform = 'translateX(-' + absPosX*100 + 'vw)';
+    absPosX += 1;
   }
-  else if(absPosX == 2) {
-    box.style.transform = 'translateX(-200vw)';
-    absPosX = 3;
-  }
-  else if(absPosX == 3) {
-    box.style.transform = 'translateX(-0vw)';
+  else {
     absPosX = 1;
+    box.style.transform = 'translateX(-' + 0 + 'vw)';
   }
 });
